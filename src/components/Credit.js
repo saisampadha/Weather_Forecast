f9c669292983bc5b1b241a5a18f75b5ba1c7134f
Sai Sampadha Vallabhuni <116197771+saisampadha@users.noreply.github.com>
2024-06-23 23:37:36 +0530

    Add files via upload

---
+++ src/components/Credit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by
+      <a href="https://github.com/saisampadha"
+        > Sai Sampadha</a
+      >, <a href="https://github.com/saisampadha/Weather_Forecast">git profile</a>
+    </p>
+  </div>
+  );
+}
